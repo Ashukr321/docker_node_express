7060883b7f1e1749c285fef93c4ab284b982fa52
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2025-04-04 00:05:06 +0530

    add mount host machine to container machine

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const port = process.env.PORT || 5500;
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).json({message:"success",status:"200",server_type:"docker node server"});
+  res.status(200).json({message:"success",status:"200",server_type:"docker node server",bugfix:" thanku sneha ji "});
 });
 
 
 app.listen(port, () => {
   console.log(`server is listening on port :${port}`);
 })
+
+// docker run --rm --name node_express -p 5500:5500 -v ${PWD}:/app docker_express
